8804d6d94f67e92c0d86604698146ec488b2420f
Madhusudhan B <madhusudhan.buragapally@wipro.com>
2016-06-16 11:41:37 +0530

    First Commit

---
+++ app/src/main/java/com/example/ex_1/EX_1_Activity.java
@@ -0,0 +1,13 @@
+package com.example.ex_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EX_1_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ex_1_);
+    }
+}
